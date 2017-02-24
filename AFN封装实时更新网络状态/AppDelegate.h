d784632974ad2487e6968db807960bcbf357474a
ZongAng123 <zongang15@yahoo.com>
2017-02-24 11:29:29 +0800

    Initial Commit

---
+++ AFN封装实时更新网络状态/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFN封装实时更新网络状态
+//
+//  Created by 纵昂 on 2017/2/24.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFN封装实时更新网络状态/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFN封装实时更新网络状态
+//
+//  Created by 纵昂 on 2017/2/24.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
